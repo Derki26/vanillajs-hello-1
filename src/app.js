2678753dd18db9584a6ac04b8388d890311d5a5e
Derki Echeverria <143278077+Derki26@users.noreply.github.com>
2023-10-07 00:58:41 +0000

    rrr

--- src/app.js
+++ src/app.js
@@ -5,8 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function excuse(){
 
+  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'the keys', 'the car'];
+  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+  
+  function randomElement(array){
+  return array[Math.floor(Math.round(Math.random() * array.length))];
+  }
+
+  let excuse = randomElement(who) + 
+  " " +
+  randomElement(action) +
+  " " + 
+  randomElement(what) +
+  " " +
+  randomElement(when);
+
+  console.log(excuse);
+  
+  const pElement = document.getElementsByTagName("p")
+  
+  pElement[0].innerText = excuse;
+  
+  console.log(pElement[0]); 
+
+};
